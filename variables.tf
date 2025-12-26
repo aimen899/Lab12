ed9a610fbd46b01f975a90e59ddf555ab620b027
aimen899 <23-22411-002@se.fjwu.edu.pk>
2025-12-26 20:27:57 +0000

    Initial commit for Lab12

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "availability_zone" {}
+variable "env_prefix" {}
+variable "instance_type" {}
+variable "public_key" {}
+variable "private_key" {}
+
